1a42fa76710adb19ddd9457d9e9d474953504709
Hossain Forhad <hossain.28forhad@gmail.com>
2025-08-30 04:52:10 +0600

    Traditional Fun vs Arrow Fun

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-// map() - calculation sesh kore notun akta array return korbe
+// Traditional function
+function dis1(){
+    console.log('Traditional Function');
+}
 
-var numbers = [2,3,4,5,6];
-var squareNumbers = numbers.map(function(x){
-    return x*x;
-})
-// console.log(squareNumbers);
+// Arrow function
+const dis2 = () => console.log('Arrow Function');
 
+// dis1();
+// dis2();
 
-// filter()
-var numbers = [2,32,40,15,6];
-var newNumbers = numbers.filter(function(x){
-    return x>10;
-})
-console.log(newNumbers);
+// Traditional fun
+function message1(){
+    return "Hi I am message1";
+}
+// Arrow fun
+const message2 = () => "hi I am from message2";
+// console.log(message1());
+// console.log(message2());
+
+// Traditional Fun
+function add1(a,b){
+    return a+b;
+}
+// Arrow Fun
+const add2 = (a,b) => a+b;
+
+console.log(add1(10,20));
+console.log(add2(10,20));
