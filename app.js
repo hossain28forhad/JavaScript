190644a68d61cfbdd156df78c337644d56e05f01
Hossain Forhad <hossain.28forhad@gmail.com>
2025-08-30 04:07:25 +0600

    Object Literals and Consize Syntext

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-// alert('alert message!');
+// Object Literals
 
+function fun1(name, age){
+    return{
+        // name: name,
+        // age: age
+        name,
+        age
+    }
+}
 
-console.log('Hello JavaScript');
+// console.log(fun1('Forhad Hossain',27));
+
+// Consize method
+let message = {
+    body(){
+        return 'Consize method';
+    }
+}
+console.log(message.body());
