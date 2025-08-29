43b00c7a94015d7624596706c0273add3c5a25bb
Hossain Forhad <hossain.28forhad@gmail.com>
2025-08-30 04:25:53 +0600

    Foreach

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-// For of
-
-// const names = ['s1','s2','s3'];
-// for(let name of names){
-//     console.log(name);
-// }
-
-
-// For in
-let students ={
-    ID : 501,
-    name: 'Jhon Doe',
-    cgpa: 3.50
-}
-for(let x in students){
-    // console.log(x);
-    // console.log(students[x]);
-    console.log(`${x} : ${students[x]}`); // string template literals
-}
+// Foreach
+var numbers = [10,20,30,40];
+numbers.forEach(function(x,index,arr){
+    arr[index] = x+5;
+})
+console.log(numbers)
