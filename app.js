eb386cfbecec438435088887a5919d09f4b766c9
Hossain Forhad <hossain.28forhad@gmail.com>
2025-08-30 06:28:08 +0600

    estructuring array and objects

--- app.js
+++ app.js
@@ -1,41 +1,65 @@
-// Arrow function (part-2) | arrow with map,filter
+// Destructuring array and objects
+// ============================
 
-var students = [
-  {
-    id: 101,
-    name: "Kamal",
-    gpa: 2.23,
-  },
-  {
-    id: 102,
-    name: "Ahmed",
-    gpa: 4.63,
-  },
-  {
-    id: 103,
-    name: "Zakir",
-    gpa: 5.00,
-  },
-  {
-    id: 104,
-    name: "Sohel",
-    gpa: 3.23,
-  }
-];
+// array destructure ============
+let numbers = [10,20,30,35,40];
+// let [num1, num2, num3, num4] = numbers;
+let [num1, num2, ...z] = numbers;
+// console.log(num1);
+// console.log(num2);
+// console.log(num1);
+// console.log(num2);
+// console.log(z);
 
-// console.log(students);
 
-// Traditional function
-function studentNames(){
-    return students.filter(function(x){
-        return x.gpa>3;
-    }).map(function(y){
-        return y.name;
-    })
+// swap variables==================
+let a=10, b=20;
+[a,b] = [b,a];
+// console.log(a);
+// console.log(b);
+
+// object destructure===========
+const studentInfo = {
+    id : 101,
+    fullName : 'Jhon Doe',
+    gpa : 3.94
 }
+// console.log(studentInfo.id);
+// console.log(studentInfo.fullName);
+// console.log(studentInfo.gpa);
+const {idd, fullName} = studentInfo;
+// console.log(idd);
+// console.log(fullName);
 
-// console.log(studentNames());
+// Nested Object destructre=============
+const studentInfo2 = {
+    id : 101,
+    fullName : 'Jhon Doe',
+    gpa : 3.94,
+    languages : {
+        native : 'bangla',
+        beginner : 'english'
+}
+}
+const {id, languages} = studentInfo2;
+// console.log(languages);
+// console.log(languages.native);
 
-// Arrow fun
-const studentNames2 = () => students.filter((x) => x.gpa> 4).map((y) =>y.name);
-console.log(studentNames2());
+// destructuring function parameters==========
+// const sInfo = (student) => {
+//    //console.log(`${student.id}, ${student.fullName}`) // es6 template literals
+// }
+// const student = {
+//     id : 101,
+//     fullName: 'Forhad Hossain'
+// }
+// sInfo(student)
+const sInfo = ({id, fullName}) => {
+   console.log(`${id}, ${fullName}`) // es6 template literals
+}
+const student = {
+    id : 1001,
+    fullName: 'Forhad Hossain'
+}
+sInfo(student)
+
